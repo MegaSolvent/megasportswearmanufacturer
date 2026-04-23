5c4ba7c8a5a735a7e291f583f79e2a87045d9176
MegaSolvent <Megasolvent10@gmail.com>
2026-04-23 20:47:06 +0500

    fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- env: {
-    GMAIL_USER: 'numanrp786@gmail.com',  // Temporary test
-    GMAIL_PASS: 'cvjy myhr jnpk jaay',
-    TO_EMAIL: 'infonuman88@gmail.com'
-  }
+
 };
 
